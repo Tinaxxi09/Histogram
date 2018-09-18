7e427b8f1e3673dc25c19d135829be6de6f785e6
Tinaxxi09 <43378405+Tinaxxi09@users.noreply.github.com>
2018-09-18 20:53:41 +0700

    Histogram of BMP image

---
+++ BMP.m
@@ -0,0 +1,14 @@
+%% HISTOGRAM
+clear;
+clc;
+i1= imread('D:\matlab class\image\1327-w.bmp');
+gr1 = rgb2gray(i1);
+his1 = imhist(gr1); %% histogram
+c1 = cumsum(his1); %% cumulation
+%%show
+figure(1),subplot(1,3,1),imshow(i1)  
+title('original image');
+figure(1),subplot(1,3,2),bar(his1,'BarWidth',1)
+title('original histogram');
+figure(1),subplot(1,3,3),bar(c1,'BarWidth',1)
+title('original cumulative histogram');
